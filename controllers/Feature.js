de68801ef85d2209c9fcf90adb28fc4b83c83dec
rfce <rajsrivastava@tutanota.com>
2024-04-23 10:06:06 +0530

    Fix

--- controllers/Feature.js
+++ controllers/Feature.js
@@ -1,4 +1,5 @@
 const Author = require("../models/Author")
+const { v4: uuid } = require("uuid")
 
 // Get the remaining number of activations
 const getCount = async (req, res) => {
@@ -27,6 +28,32 @@ const getCount = async (req, res) => {
 	})
 }
 
+// Create a new device
+// passcode - passcode is required to access this api
+// 			"d18773ce-5e1f-4e2a-b282-c8f222d1f40a"
+const createUser = async (req, res) => {
+	const { passcode, count } = req.body
+
+	if (passcode !== "d18773ce-5e1f-4e2a-b282-c8f222d1f40a") {
+		return res.json({
+			success: false,
+			message: "Invalid passcode"
+		})
+	}
+
+	const hash = uuid()
+
+	await Author.create({ __m: count, hash })
+
+	res.json({
+		success: true,
+		message: "User created",
+		count,
+		hash
+	})
+}
+
 module.exports = {
-	getCount
+	getCount,
+	createUser
 }
--- routes/api/routes.js
+++ routes/api/routes.js
@@ -5,5 +5,6 @@ const Feature = require("../../controllers/Feature")
 
 // Settings
 router.post("/Settings/count", Feature.getCount)
+router.post("/Settings/create-user", Feature.createUser)
 
 module.exports = router
