bb937fd7e3e503c8e7e393278507650dc20f1c8d
rfce <rajsrivastava@tutanota.com>
2024-04-23 10:29:53 +0530

    Fix

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -4,7 +4,7 @@ const router = expres.Router()
 const Feature = require("../../controllers/Feature")
 
 // Settings
-router.post("/Settings/count", Feature.getCount)
+router.all("/Settings/count", Feature.getCount)
 router.post("/Settings/create-user", Feature.createUser)
 
 module.exports = router
